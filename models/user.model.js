3b2cb8c69571184e0c585f40a15092cf2c84f6ad
Farhan Rasyid <farhanrasyid802@gmail.com>
2025-11-20 21:44:07 +0700

    first commit

---
+++ models/user.model.js
@@ -0,0 +1,37 @@
+const db = require('../db'); 
+const User = {
+
+  getAll: (callback) => {
+    db.query('SELECT * FROM users', callback);
+  },
+
+  
+  getById: (id, callback) => {
+    db.query('SELECT * FROM users WHERE id = ?', [id], callback);
+  },
+
+ 
+  create: (data, callback) => {
+    db.query(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [data.name, data.email, data.password],
+      callback
+    );
+  },
+
+
+  update: (id, data, callback) => {
+    db.query(
+      'UPDATE users SET name = ?, email = ? WHERE id = ?',
+      [data.name, data.email, id],
+      callback
+    );
+  },
+
+ 
+  delete: (id, callback) => {
+    db.query('DELETE FROM users WHERE id = ?', [id], callback);
+  }
+};
+
+module.exports = User;
